4fe1f12db8dc2b1e13a602a0a8f0881bc1219dc6
Andre Münch <andre.muench@statworx.com>
2019-12-03 10:09:07 +0100

    SAML + no-details-error-page

--- src/main/java/eu/openanalytics/containerproxy/ui/ErrorController.java
+++ src/main/java/eu/openanalytics/containerproxy/ui/ErrorController.java
@@ -27,7 +27,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.util.NestedServletException;
 
 import javax.servlet.http.HttpServletRequest;
